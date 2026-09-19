c6b2bc928d74edec971474bdb88d3617acf1c678
Abdulsalam mustapha <abdulsalammustapha72@icloud.com>
2026-09-19 12:09:38 +0100

    Change API base URL to production server

--- src/api/Axios.jsx
+++ src/api/Axios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api'
+    baseURL: 'https://personal-website-0e8g.onrender.com'
 });
 
 export default api;
