c8927231d3702374099a7675bfe753b335129901
Abdulsalam mustapha <abdulsalammustapha72@icloud.com>
2026-09-19 12:13:27 +0100

    Update baseURL to include '/api' path

--- src/api/Axios.jsx
+++ src/api/Axios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://personal-website-0e8g.onrender.com'
+    baseURL: 'https://personal-website-0e8g.onrender.com/api'
 });
 
 export default api;
